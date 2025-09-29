a1c7609bae0b308bb74e16e0ab318a0f1406722d
Mochi <ps.rabr.013177@gmail.com>
2025-09-29 01:27:13 -0500

    docs: agrega comentarios explicativos en código (API, Dockerfile, backup, healthcheck)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//se monta en StrictMode, lo cual ejecuta doble render en dev.
+// Esto se hace intencionalmente para detectar efectos secundarios ocultos
+// por ejemplo, llamadas a APIs que no están correctamente controladas
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
